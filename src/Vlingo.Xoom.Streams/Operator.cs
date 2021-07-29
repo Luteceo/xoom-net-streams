b2ff8579676374df59d67f52ba6c8124647a1e83
Hamza JGUERIM <hamzajg@gmail.com>
2021-07-29 18:53:53 +0100

    Add Mapper Operator..
    Signed-off-by: Hamza JGUERIM <hamzajg@gmail.com>

--- src/Vlingo.Xoom.Streams/Operator.cs
+++ src/Vlingo.Xoom.Streams/Operator.cs
@@ -6,6 +6,7 @@ namespace Vlingo.Xoom.Streams
   public abstract class Operator<T, R>
   {
     public static Operator<T, T> FilterWith(Predicate<T> filter) => new Filter<T>(filter);
+    public static Operator<T, R> MapWith(Func<T, R> mapper) => new Mapper<T, R>(mapper);
     public abstract void PerformInto(T value, Action<R> consumer);
   }
 }
