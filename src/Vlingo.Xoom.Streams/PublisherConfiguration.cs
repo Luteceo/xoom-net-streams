1814b54754df8a98ca6ec8e51b5356afadd62553
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-08-04 19:40:10 +0200

    Fix unsported C# features errors

--- src/Vlingo.Xoom.Streams/PublisherConfiguration.cs
+++ src/Vlingo.Xoom.Streams/PublisherConfiguration.cs
@@ -14,11 +14,11 @@ namespace Vlingo.Xoom.Streams
     public readonly int ProbeInterval;
 
     public static PublisherConfiguration DefaultDropHead =>
-      new(DefaultProbeInterval, DefaultMaxThrottle, DefaultBufferSize,
+      new PublisherConfiguration(DefaultProbeInterval, DefaultMaxThrottle, DefaultBufferSize,
         OverflowPolicy.DropHead);
 
     public static PublisherConfiguration DefaultDropCurrent =>
-      new(DefaultProbeInterval, DefaultMaxThrottle, DefaultBufferSize,
+      new PublisherConfiguration(DefaultProbeInterval, DefaultMaxThrottle, DefaultBufferSize,
         OverflowPolicy.DropCurrent);
 
     public PublisherConfiguration(int probeInterval, int maxThrottle, int bufferSize, OverflowPolicy overflowPolicy)
