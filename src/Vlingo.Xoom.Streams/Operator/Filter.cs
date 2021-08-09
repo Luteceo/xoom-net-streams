9bd5fc44244077850d4eca96629ada3057ee966b
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-08-09 19:44:49 +0200

    Formatting

--- src/Vlingo.Xoom.Streams/Operator/Filter.cs
+++ src/Vlingo.Xoom.Streams/Operator/Filter.cs
@@ -1,27 +1,33 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
 using System;
 
 namespace Vlingo.Xoom.Streams.Operator
 {
-  public class Filter<T> : Operator<T, T>
-  {
-    private readonly Predicate<T> _predicate;
-
-    public Filter(Predicate<T> predicate)
+    public class Filter<T> : Operator<T, T>
     {
-      _predicate = predicate;
-    }
+        private readonly Predicate<T> _predicate;
 
-    public override void PerformInto(T value, Action<T> consumer)
-    {
-      try
-      {
-        if (_predicate.Invoke(value))
-          consumer.Invoke(value);
-      }
-      catch (Exception e)
-      {
-        Streams.Logger.Error($"Filter failed because: {e.Message}", e);
-      }
+        public Filter(Predicate<T> predicate) => _predicate = predicate;
+
+        public override void PerformInto(T value, Action<T> consumer)
+        {
+            try
+            {
+                if (_predicate.Invoke(value))
+                {
+                    consumer.Invoke(value);
+                }
+            }
+            catch (Exception e)
+            {
+                Streams.Logger.Error($"Filter failed because: {e.Message}", e);
+            }
+        }
     }
-  }
 }
