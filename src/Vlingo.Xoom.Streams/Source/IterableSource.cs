0d8610cda589826ef9f969825da8cb2033d7de4e
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-08-10 21:43:05 +0200

    Formatting, commenting, docs..

--- src/Vlingo.Xoom.Streams/Source/IterableSource.cs
+++ src/Vlingo.Xoom.Streams/Source/IterableSource.cs
@@ -1,46 +1,44 @@
-﻿using System.Collections.Generic;
+﻿// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System.Collections.Generic;
 using Vlingo.Xoom.Common;
 
 namespace Vlingo.Xoom.Streams.Source
 {
-  public class IterableSource<T> : Source<T>
-  {
-    private readonly IEnumerator<T> _iterator;
-    private readonly bool _slowIterable;
-
-    public IterableSource(IEnumerable<T> iterator, bool slowIterable)
+    public class IterableSource<T> : Source<T>
     {
-      _iterator = iterator.GetEnumerator();
-      _slowIterable = slowIterable;
-    }
+        private readonly IEnumerator<T> _iterator;
+        private readonly bool _slowIterable;
 
-    public override ICompletes<Elements<T>> Next()
-    {
-      if (!_iterator.MoveNext()) return Completes.WithSuccess(new Elements<T>(new T[0], false));
+        public IterableSource(IEnumerable<T> iterator, bool slowIterable)
+        {
+            _iterator = iterator.GetEnumerator();
+            _slowIterable = slowIterable;
+        }
 
-      var elements = new T[1];
-      elements[0] = _iterator.Current;
-      return Completes.WithSuccess(new Elements<T>(elements, false));
-    }
+        public override ICompletes<Elements<T>> Next()
+        {
+            if (!_iterator.MoveNext())
+            {
+                return Completes.WithSuccess(new Elements<T>(new T[0], false));
+            }
 
-    public override ICompletes<Elements<T>> Next(int maximumElements)
-    {
-      return Next();
-    }
+            var elements = new T[1];
+            elements[0] = _iterator.Current;
+            return Completes.WithSuccess(new Elements<T>(elements, false));
+        }
 
-    public override ICompletes<Elements<T>> Next(long index)
-    {
-      return Next();
-    }
+        public override ICompletes<Elements<T>> Next(int maximumElements) => Next();
 
-    public override ICompletes<Elements<T>> Next(long index, int maximumElements)
-    {
-      return Next();
-    }
+        public override ICompletes<Elements<T>> Next(long index) => Next();
 
-    public override ICompletes<bool> IsSlow()
-    {
-      return Completes.WithSuccess(_slowIterable);
+        public override ICompletes<Elements<T>> Next(long index, int maximumElements) => Next();
+
+        public override ICompletes<bool> IsSlow() => Completes.WithSuccess(_slowIterable);
     }
-  }
 }
--- src/Vlingo.Xoom.Streams/Source/LongRangeSource.cs
+++ src/Vlingo.Xoom.Streams/Source/LongRangeSource.cs
@@ -1,51 +1,52 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System.Diagnostics.Contracts;
 using Vlingo.Xoom.Common;
 
 namespace Vlingo.Xoom.Streams.Source
 {
-  public class LongRangeSource : Source<long>
-  {
-    private readonly long _startInclusive;
-    private readonly long _endExclusive;
-    private long _current;
-
-    public LongRangeSource(long startInclusive, long endExclusive)
+    public class LongRangeSource : Source<long>
     {
-      // Assert(startInclusive <= endExclusive)
-      // Assert(startInclusive >= 0 && startInclusive <= long.Max)
-      _startInclusive = startInclusive;
-      // Assert(endExclusive >= 0 && endExclusive <= long.Max)
-      _endExclusive = endExclusive;
+        private readonly long _startInclusive;
+        private readonly long _endExclusive;
+        private long _current;
 
-      _current = startInclusive;
-    }
+        public LongRangeSource(long startInclusive, long endExclusive)
+        {
+            Contract.Assert(startInclusive <= endExclusive);
+            Contract.Assert(startInclusive >= 0 && startInclusive <= long.MaxValue);
+            _startInclusive = startInclusive;
+            Contract.Assert(endExclusive >= 0 && endExclusive <= long.MaxValue);
+            _endExclusive = endExclusive;
 
-    public override ICompletes<Elements<long>> Next()
-    {
-      if (_current >= _endExclusive)
-        return Completes.WithSuccess(new Elements<long>(new long[0], true));
-      var elements = new long[1];
-      elements[0] = _current++;
-      return Completes.WithSuccess(new Elements<long>(elements, false));
-    }
+            _current = startInclusive;
+        }
 
-    public override ICompletes<Elements<long>> Next(int maximumElements)
-    {
-      return Next();
-    }
+        public override ICompletes<Elements<long>> Next()
+        {
+            if (_current >= _endExclusive)
+            {
+                return Completes.WithSuccess(new Elements<long>(new long[0], true));
+            }
+            var elements = new long[1];
+            elements[0] = _current++;
+            return Completes.WithSuccess(new Elements<long>(elements, false));
+        }
 
-    public override ICompletes<Elements<long>> Next(long index)
-    {
-      return Next();
-    }
+        public override ICompletes<Elements<long>> Next(int maximumElements) => Next();
 
-    public override ICompletes<Elements<long>> Next(long index, int maximumElements)
-    {
-      return Next();
-    }
+        public override ICompletes<Elements<long>> Next(long index) => Next();
 
-    public override ICompletes<bool> IsSlow()
-    {
-      return Completes.WithSuccess(false);
+        public override ICompletes<Elements<long>> Next(long index, int maximumElements) => Next();
+
+        public override ICompletes<bool> IsSlow() => Completes.WithSuccess(false);
+
+        public override string ToString() => 
+            $"LongRangeSource [startInclusive={_startInclusive} endExclusive={_endExclusive} current={_current}]";
     }
-  }
 }
