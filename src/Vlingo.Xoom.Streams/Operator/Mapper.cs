c702feedbeeb482c4846c4aacf0e0ac450fd33e5
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-08-10 20:36:41 +0200

    Formatting, commenting, docs.. Implement TestThatMapperFlatMapsOptionals

--- src/Vlingo.Xoom.Streams/Operator/Mapper.cs
+++ src/Vlingo.Xoom.Streams/Operator/Mapper.cs
@@ -1,26 +1,35 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
 using System;
 
 namespace Vlingo.Xoom.Streams.Operator
 {
-  public class Mapper<T, R> : Operator<T, R>
-  {
-    private readonly Func<T, R> _mapper;
-
-    public Mapper(Func<T, R> mapper)
+    /// <summary>
+    /// Maps from <see cref="Sink{T}"/> to <see cref="Source{T}"/>
+    /// </summary>
+    /// <typeparam name="T">The input parameter type</typeparam>
+    /// <typeparam name="TR">The result type</typeparam>
+    public class Mapper<T, TR> : Operator<T, TR>
     {
-      _mapper = mapper;
-    }
+        private readonly Func<T, TR> _mapper;
 
-    public override void PerformInto(T value, Action<R> consumer)
-    {
-      try
-      {
-        consumer.Invoke(_mapper.Invoke(value));
-      }
-      catch (Exception e)
-      {
-        Streams.Logger.Error($"Mapper failed because: {e.Message}", e);
-      }
+        public Mapper(Func<T, TR> mapper) => _mapper = mapper;
+
+        public override void PerformInto(T value, Action<TR> consumer)
+        {
+            try
+            {
+                consumer.Invoke(_mapper.Invoke(value));
+            }
+            catch (Exception e)
+            {
+                Streams.Logger.Error($"Mapper failed because: {e.Message}", e);
+            }
+        }
     }
-  }
 }
