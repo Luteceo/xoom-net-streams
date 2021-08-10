6674b35e60c5b8179d1d317d72e7c6ba63673cbe
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-08-10 21:43:17 +0200

    Implement SupplierSource

---
+++ src/Vlingo.Xoom.Streams/Source/SupplierSource.cs
@@ -0,0 +1,50 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System;
+using Vlingo.Xoom.Common;
+
+namespace Vlingo.Xoom.Streams.Source
+{
+    /// <summary>
+    /// A Source that uses a <see cref="Func{TResult}"/> to provide next <see cref="Elements{T}"/> instances.
+    /// </summary>
+    /// <typeparam name="T">The T type of Element being supplied</typeparam>
+    public class SupplierSource<T> : Source<T>
+    {
+        private readonly bool _slowSupplier;
+        private readonly Func<T> _supplier;
+        
+        public SupplierSource(Func<T> supplier, bool slowSupplier) 
+        {
+            _supplier = supplier;
+            _slowSupplier = slowSupplier;
+        }
+        
+        public override ICompletes<Elements<T>> Next()
+        {
+            var any = _supplier();
+
+            if (any != null)
+            {
+                var elements = new T[1];
+                elements[0] = any;
+                return Completes.WithSuccess(new Elements<T>(elements, false));
+            }
+
+            return Completes.WithSuccess(new Elements<T>(new T[0], true));
+        }
+
+        public override ICompletes<Elements<T>> Next(int maximumElements) => Next();
+
+        public override ICompletes<Elements<T>> Next(long index) => Next();
+
+        public override ICompletes<Elements<T>> Next(long index, int maximumElements) => Next();
+
+        public override ICompletes<bool> IsSlow() => Completes.WithSuccess(_slowSupplier);
+    }
+}
