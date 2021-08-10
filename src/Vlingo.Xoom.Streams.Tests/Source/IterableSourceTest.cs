850cb72ca6e61ed156d78881ae9439cd9e1af54a
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-08-10 22:57:35 +0200

    Formatting, commenting, docs..

--- src/Vlingo.Xoom.Streams.Tests/Source/IterableSourceTest.cs
+++ src/Vlingo.Xoom.Streams.Tests/Source/IterableSourceTest.cs
@@ -1,37 +1,44 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
 using Xunit;
 
 namespace Vlingo.Xoom.Streams.Tests.Source
 {
-  public class IterableSourceTest : SourceTest
-  {
-    [Fact]
-    public void TestThatEmptyHasNoElements()
+    public class IterableSourceTest : SourceTest
     {
-      var source = Source<string>.Empty();
+        [Fact]
+        public void TestThatEmptyHasNoElements()
+        {
+            var source = Source<string>.Empty();
 
-      var result = StringFromSource(source);
+            var result = StringFromSource(source);
 
-      Assert.True(string.IsNullOrEmpty(result));
+            Assert.True(string.IsNullOrEmpty(result));
+        }
+
+        [Fact]
+        public void TestThatSourceProvidesElements()
+        {
+            var source = Source<string>.Only(new[] { "A", "B", "C" });
+
+            var result = StringFromSource(source);
+
+            Assert.Equal("ABC", result);
+        }
+
+        [Fact]
+        public void TestThatSourceCollectionProvidesElements()
+        {
+            var source = Source<string>.With(new[] { "A", "B", "C" });
+
+            var result = StringFromSource(source);
+
+            Assert.Equal("ABC", result);
+        }
     }
-
-    [Fact]
-    public void TestThatSourceProvidesElements()
-    {
-      var source = Source<string>.Only(new[] {"A", "B", "C"});
-
-      var result = StringFromSource(source);
-
-      Assert.Equal("ABC", result);
-    }
-
-    [Fact]
-    public void TestThatSourceCollectionProvidesElements()
-    {
-      var source = Source<string>.With(new[] {"A", "B", "C"});
-
-      var result = StringFromSource(source);
-
-      Assert.Equal("ABC", result);
-    }
-  }
 }
