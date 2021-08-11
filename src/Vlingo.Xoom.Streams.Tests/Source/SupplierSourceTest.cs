1c479828e2d4a7bc48461420fa452069e3a77d22
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-08-11 21:37:56 +0200

    Implement SupplierSourceTest

---
+++ src/Vlingo.Xoom.Streams.Tests/Source/SupplierSourceTest.cs
@@ -0,0 +1,38 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System;
+using Xunit;
+
+namespace Vlingo.Xoom.Streams.Tests.Source
+{
+    public class SupplierSourceTest : SourceTest
+    {
+        private int _index;
+        
+        [Fact]
+        public void TestThatSourceSupplierProvidesElements()
+        {
+            _index = 0;
+
+            Func<string> supplier = () =>
+            {
+                if (_index >= 3) return null;
+                var next = (char) ('A' + _index);
+                var value = next.ToString();
+                ++_index;
+                return value;
+            };
+
+            var source = Source<string>.With(supplier);
+
+            var result = StringFromSource(source);
+
+            Assert.Equal("ABC", result);
+        }
+    }
+}
