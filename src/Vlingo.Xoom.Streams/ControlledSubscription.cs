da4c08c62d0eeafb5bca4aeda76d3cc86d804a4d
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-08-11 21:43:48 +0200

    Formatting, commenting, docs..

--- src/Vlingo.Xoom.Streams/ControlledSubscription.cs
+++ src/Vlingo.Xoom.Streams/ControlledSubscription.cs
@@ -1,8 +1,15 @@
-﻿namespace Vlingo.Xoom.Streams
+﻿// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+namespace Vlingo.Xoom.Streams
 {
-  public interface IControlledSubscription<T>
-  {
-    public void Cancel(SubscriptionController<T> subscription);
-    public void Request(SubscriptionController<T> subscription, long maximum);
-  }
+    public interface IControlledSubscription<T>
+    {
+        public void Cancel(SubscriptionController<T> subscription);
+        public void Request(SubscriptionController<T> subscription, long maximum);
+    }
 }
