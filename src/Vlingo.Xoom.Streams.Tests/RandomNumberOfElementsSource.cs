703114e2a0376856c4ba16f7f3037b05cde50d1d
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-08-16 01:03:15 +0200

    Formatting, commenting, docs..

--- src/Vlingo.Xoom.Streams.Tests/RandomNumberOfElementsSource.cs
+++ src/Vlingo.Xoom.Streams.Tests/RandomNumberOfElementsSource.cs
@@ -1,61 +1,63 @@
-﻿using System;
+﻿// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System;
 using Vlingo.Xoom.Common;
 
 namespace Vlingo.Xoom.Streams.Tests
 {
-  class RandomNumberOfElementsSource : Source<string>
-  {
-    private readonly AtomicInteger _element = new AtomicInteger(0);
-    private readonly Random _count = new Random();
-    private readonly int _total;
-    private readonly bool _slow;
-
-    public RandomNumberOfElementsSource(int total) : this(total, false)
+    internal class RandomNumberOfElementsSource : Source<string>
     {
+        private readonly AtomicInteger _element = new AtomicInteger(0);
+        private readonly Random _count = new Random();
+        private readonly int _total;
+        private readonly bool _slow;
+
+        public RandomNumberOfElementsSource(int total) : this(total, false)
+        {
+        }
+
+        public RandomNumberOfElementsSource(int total, bool slow)
+        {
+            _total = total;
+            _slow = slow;
+        }
+
+        public override ICompletes<bool> IsSlow() => Completes.WithSuccess(_slow);
+
+        public override ICompletes<Elements<string>> Next()
+        {
+            var current = _element.Get();
+            if (current >= _total)
+            {
+                return Completes.WithSuccess(new Elements<string>(new string[0], true));
+            }
+
+            var next = RandomNumberOfElements(current);
+            for (var idx = 0; idx < next.Length; ++idx)
+            {
+                next[idx] = $"{_element.IncrementAndGet()}";
+            }
+
+            return Completes.WithSuccess(new Elements<string>(next, false));
+        }
+
+        private string[] RandomNumberOfElements(int current)
+        {
+            var bound = current > _total - 10 && current < _total ? _total - current : 7;
+            var number = _count.Next(bound);
+            var next = new string[number <= 0 ? 1 : number];
+            return next;
+        }
+
+        public override ICompletes<Elements<string>> Next(int maximumElements) => Next();
+
+        public override ICompletes<Elements<string>> Next(long index) => Next();
+
+        public override ICompletes<Elements<string>> Next(long index, int maximumElements) => Next();
     }
-
-    public RandomNumberOfElementsSource(int total, bool slow)
-    {
-      _total = total;
-      _slow = slow;
-    }
-
-    public override ICompletes<bool> IsSlow() => Completes.WithSuccess(_slow);
-
-    public override ICompletes<Elements<string>> Next()
-    {
-      var current = _element.Get();
-      if (current >= _total)
-        return Completes.WithSuccess(new Elements<string>(new string[0], true));
-
-      var next = RandomNumberOfElements(current);
-      for (var idx = 0; idx < next.Length; ++idx)
-        next[idx] = $"{_element.IncrementAndGet()}";
-
-      return Completes.WithSuccess(new Elements<string>(next, false));
-    }
-
-    private string[] RandomNumberOfElements(int current)
-    {
-      var bound = (current > _total - 10 && current < _total) ? (_total - current) : 7;
-      var number = _count.Next(bound);
-      var next = new string[number <= 0 ? 1 : number];
-      return next;
-    }
-
-    public override ICompletes<Elements<string>> Next(int maximumElements)
-    {
-      return Next();
-    }
-
-    public override ICompletes<Elements<string>> Next(long index)
-    {
-      return Next();
-    }
-
-    public override ICompletes<Elements<string>> Next(long index, int maximumElements)
-    {
-      return Next();
-    }
-  }
 }
--- src/Vlingo.Xoom.Streams.Tests/StreamProcessorTest.cs
+++ src/Vlingo.Xoom.Streams.Tests/StreamProcessorTest.cs
@@ -1,43 +1,83 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
 using System.Collections.Generic;
+using System.Linq;
 using Reactive.Streams;
 using Vlingo.Xoom.Streams.Sink.Test;
 using Xunit;
 
 namespace Vlingo.Xoom.Streams.Tests
 {
-  public class StreamProcessorTest : StreamPubSubTest
-  {
-    private readonly SafeConsumerSink<int> _sink;
-    private readonly StringToIntegerMapper _transformer;
-
-    [Fact]
-    public void TestThatProcessorPipesTransformation()
+    public class StreamProcessorTest : StreamPubSubTest
     {
-      // GIVEN
-      CreatePublisherWith(SourceOf123);
-      var access = _transformer.AfterCompleting(6);
-      var subscriber = CreateSubscriberWithoutSubscribing(_sink, 3);
-      var processor = World.ActorFor(new[] {typeof(IProcessor<string, int>)}, typeof(StreamProcessor<string, int>),
-        _transformer, 3L, PublisherConfiguration.DefaultDropHead)
-        .Get<IProcessor<string, int>>(0);
+        private readonly SafeConsumerSink<int> _sink;
+        private readonly StringToIntegerMapper _transformer;
 
-      // WHEN
-      processor.Subscribe(subscriber);
-      Publisher.Subscribe(processor);
+        [Fact]
+        public void TestThatProcessorPipesTransformation()
+        {
+            CreatePublisherWith(SourceOf123);
+            var access = _transformer.AfterCompleting(6);
+            var subscriber = CreateSubscriberWithoutSubscribing(_sink, 3);
+            var processor = World.ActorFor(new[] { typeof(IProcessor<string, int>) },
+                    typeof(StreamProcessor<string, int>),
+                    _transformer, 3L, PublisherConfiguration.DefaultDropHead)
+                .Get<IProcessor<string, int>>(0);
+            
+            processor.Subscribe(subscriber);
+            Publisher.Subscribe(processor);
 
-      // THEN
-      var transformCount = access.ReadFrom<int>("transformCount");
-      Assert.Equal(3, transformCount);
+            var transformCount = access.ReadFrom<int>("transformCount");
+            Assert.Equal(3, transformCount);
 
-      var values = access.ReadFrom<List<int>>("values");
-      var expected = IntegerListOf1To(transformCount);
-      Assert.Equal(expected, values);
+            var values = access.ReadFrom<List<int>>("values");
+            var expected = IntegerListOf1To(transformCount);
+            Assert.Equal(expected, values);
+        }
+        
+        [Fact]
+        public void TestThatProcessorPipesTransformationOfMany()
+        {
+            CreatePublisherWith(SourceRandomNumberOfElements);
+
+            var accessTransformer = _transformer.AfterCompleting(200);
+
+            var accessSink = _sink.AfterCompleting(102);
+
+            var subscriber = CreateSubscriberWithoutSubscribing(_sink, 10);
+
+            var processor = World.ActorFor<IProcessor<string, int>>(() => new StreamProcessor<string, int>(_transformer, 10, PublisherConfiguration.DefaultDropHead));
+
+            processor.Subscribe(subscriber);
+
+            Publisher.Subscribe(processor);
+
+            var sinkValue = _sink.AccessValueMustBe("value", 100);
+
+            Assert.Equal(100, sinkValue);
+
+            var transformCount = accessTransformer.ReadFrom<int>("transformCount");
+
+            Assert.Equal(100, transformCount);
+
+            var values = accessTransformer.ReadFrom<IEnumerable<int>>("values").ToList();
+            var expected = IntegerListOf1To(transformCount).ToList();
+
+            for (var i = 0; i < values.Count; i++)
+            {
+                Assert.Equal(expected[i], values[i]);
+            }
+        }
+
+        public StreamProcessorTest()
+        {
+            _sink = new SafeConsumerSink<int>();
+            _transformer = new StringToIntegerMapper();
+        }
     }
-
-    public StreamProcessorTest()
-    {
-      _sink = new SafeConsumerSink<int>();
-      _transformer = new StringToIntegerMapper();
-    }
-  }
 }
