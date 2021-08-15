95db84dc71317d005fbf01b66f2960e45ded4d90
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-08-16 00:43:36 +0200

    Implement ITransformer

---
+++ src/Vlingo.Xoom.Streams/ITransformer.cs
@@ -0,0 +1,26 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using Vlingo.Xoom.Common;
+
+namespace Vlingo.Xoom.Streams
+{
+    /// <summary>
+    /// Transform a value of type <typeparamref name="T"/> to a value of type <typeparamref name="TR"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of the original value</typeparam>
+    /// <typeparam name="TR">The type of the resulting value</typeparam>
+    public interface ITransformer<in T, TR>
+    {
+        /// <summary>
+        /// Answer the <see cref="ICompletes"/> value after transforming it from the <paramref name="value"/> of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <param name="value">The T typed original value</param>
+        /// <returns><see cref="ICompletes"/></returns>
+        ICompletes<TR> Transform(T value);
+    }
+}
---
+++ src/Vlingo.Xoom.Streams/StreamTransformer.cs
@@ -0,0 +1,21 @@
+// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using Vlingo.Xoom.Actors;
+using Vlingo.Xoom.Common;
+
+namespace Vlingo.Xoom.Streams
+{
+    public class StreamTransformer<T, TR> : Actor, ITransformer<T, TR>
+    {
+        private ITransformer<T, TR> _transformer;
+
+        public StreamTransformer(ITransformer<T, TR> transformer) => this._transformer = transformer;
+
+        public ICompletes<TR> Transform(T value) => _transformer.Transform(value);
+    }
+}
