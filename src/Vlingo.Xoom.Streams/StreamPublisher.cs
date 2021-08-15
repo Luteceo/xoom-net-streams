be35645ac5a9fba97d500151d3903bb4cb86fa7d
Tomasz Jaskula <thomasv1000@hotmail.fr>
2021-08-16 00:01:39 +0200

    Formatting, commenting, docs..

--- src/Vlingo.Xoom.Streams/StreamPublisher.cs
+++ src/Vlingo.Xoom.Streams/StreamPublisher.cs
@@ -1,48 +1,65 @@
-﻿using System;
+﻿// Copyright © 2012-2021 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System;
 using Reactive.Streams;
 using Vlingo.Xoom.Actors;
 using Vlingo.Xoom.Common;
 
 namespace Vlingo.Xoom.Streams
 {
-  public sealed class StreamPublisher<T> : Actor, IPublisher<T>, IControlledSubscription<T>, IScheduled<object>
-  {
-    private readonly StreamPublisherDelegate<T> _delegate;
-
-    public StreamPublisher(Source<T> source, PublisherConfiguration configuration)
+    public sealed class StreamPublisher<T> : Actor, IPublisher<T>, IControlledSubscription<T>, IScheduled<object>
     {
-      _delegate = new StreamPublisherDelegate<T>(source, configuration, SelfAs<IControlledSubscription<T>>(),
-        Scheduler, SelfAs<IScheduled<object>>(), SelfAs<IStoppable>());
+        private readonly StreamPublisherDelegate<T> _delegate;
+
+        public StreamPublisher(Source<T> source, PublisherConfiguration configuration) =>
+            _delegate = new StreamPublisherDelegate<T>(source, configuration, SelfAs<IControlledSubscription<T>>(),
+                Scheduler, SelfAs<IScheduled<object>>(), SelfAs<IStoppable>());
+        
+        //===================================
+        // Scheduled
+        //===================================
+        
+        public void IntervalSignal(IScheduled<object> scheduled, object data) => _delegate.ProcessNext();
+
+        //===================================
+        // ControlledSubscription
+        //===================================
+        
+        public void Request(SubscriptionController<T> subscription, long maximum)
+        {
+            Console.WriteLine($"{GetType()} : {nameof(Request)}");
+            _delegate.Request(subscription, maximum);
+        }
+        
+        public void Cancel(SubscriptionController<T> subscription) => _delegate.Cancel(subscription);
+
+        //===================================
+        // Publisher
+        //===================================
+        
+        public void Subscribe(ISubscriber<T> subscriber)
+        {
+            Console.WriteLine($"2: {GetType()} : {nameof(Subscribe)}");
+            if (subscriber != null)
+            {
+                _delegate.Subscribe(subscriber);
+            }
+        }
+
+        //===================================
+        // Internal implementation
+        //===================================
+        
+        public override void Stop()
+        {
+            _delegate.Stop();
+
+            base.Stop();
+        }
     }
-
-    public void Cancel(SubscriptionController<T> subscription)
-    {
-      _delegate.Cancel(subscription);
-    }
-
-    public void IntervalSignal(IScheduled<object> scheduled, object data)
-    {
-      _delegate.ProcessNext();
-    }
-
-    public void Request(SubscriptionController<T> subscription, long maximum)
-    {
-      Console.WriteLine($"{GetType()} : {nameof(Request)}");
-
-      _delegate.Request(subscription, maximum);
-    }
-
-    public void Subscribe(ISubscriber<T> subscriber)
-    {
-      Console.WriteLine($"2: {GetType()} : {nameof(Subscribe)}");
-      _delegate.Subscribe(subscriber);
-    }
-
-    public override void Stop()
-    {
-      _delegate.Stop();
-
-      base.Stop();
-    }
-  }
 }
